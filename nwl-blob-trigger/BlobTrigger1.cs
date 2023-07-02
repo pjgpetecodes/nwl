590fb40e05277f9bcb30d73addc5074188aa1ee1
Peter Gallagher <pete@pjgcreations.co.uk>
2023-07-02 09:29:10 +0100

    Switch to Event Driven Blob Trigger

--- nwl-blob-trigger/BlobTrigger1.cs
+++ nwl-blob-trigger/BlobTrigger1.cs
@@ -9,7 +9,7 @@ namespace nwl.blobtrigger
     public class BlobTrigger1
     {
         [FunctionName("BlobTrigger1")]
-        public void Run([BlobTrigger("uploads/{name}", Connection = "nwlstorage1_STORAGE")]Stream myBlob, string name, ILogger log)
+        public static void Run([BlobTrigger("uploads/{name}", Source = BlobTriggerSource.EventGrid, Connection = "nwlstorage1_STORAGE")]Stream myBlob, string name, ILogger log)
         {
             log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
         }
