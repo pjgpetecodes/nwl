7fda08df6699c9224838f3251c3297774045b965
Peter Gallagher <pete@pjgcreations.co.uk>
2023-07-03 08:35:07 +0100

    Added code to Blob Trigger and Service Bus Trigger functions

--- nwl-blob-trigger/BlobTrigger1.cs
+++ nwl-blob-trigger/BlobTrigger1.cs
@@ -3,15 +3,32 @@ using System.IO;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Microsoft.Azure.WebJobs.ServiceBus;
+using Azure.Messaging.ServiceBus;
+using System.Text;
 
 namespace nwl.blobtrigger
 {
     public class BlobTrigger1
     {
         [FunctionName("BlobTrigger1")]
-        public static void Run([BlobTrigger("uploads/{name}", Source = BlobTriggerSource.EventGrid, Connection = "nwlstorage1_STORAGE")]Stream myBlob, string name, ILogger log)
+        
+        public static void Run([BlobTrigger("uploads/{name}", Source = BlobTriggerSource.EventGrid, Connection = "nwlstorage1_STORAGE")]Stream myBlob, String name, 
+                                [ServiceBus("%ServiceBusQueueName%", Connection = "ServiceBusConnection", EntityType = ServiceBusEntityType.Queue)]out string queueMessage, ILogger log)
         {
             log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+
+            queueMessage = GetBlobInfo(myBlob, name);
+
+        }
+
+        public static string GetBlobInfo(Stream myBlob, string name)
+        {
+            string blobInfo = "";
+            blobInfo += "Blob Name: " + name + "\n";
+            blobInfo += "Blob Size: " + myBlob.Length + " Bytes\n";
+            
+            return blobInfo;
         }
     }
 }
--- nwl-chunker/ServiceBusQueueTrigger1.cs
+++ nwl-chunker/ServiceBusQueueTrigger1.cs
@@ -1,16 +1,40 @@
 using System;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.EventHubs;
+using Azure.Messaging.EventHubs;
 using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Threading.Tasks;
 
 namespace nwl.chunker
 {
-    public class ServiceBusQueueTrigger1
+    public static class ServiceBusQueueTrigger1
     {
         [FunctionName("ServiceBusQueueTrigger1")]
-        public void Run([ServiceBusTrigger("nwlnewfilequeue", Connection = "nwlsb1_SERVICEBUS")]string myQueueItem, ILogger log)
+         public static async Task Run([ServiceBusTrigger("nwlnewfilequeue", Connection = "nwlsb1_SERVICEBUS")]string myQueueItem, 
+                            [EventHub("%nwleventhub1_azurefunctionsender_EVENTHUB_name%", Connection = "nwleventhub1_RootManageSharedAccessKey_EVENTHUB")] IAsyncCollector<Azure.Messaging.EventHubs.EventData> outputEvents,
+                            ILogger log)
         {
             log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+
+            string storageAccountConnectionString = Environment.GetEnvironmentVariable("nwlstorage1_STORAGE");            
+
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageAccountConnectionString);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference("uploads");
+
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(myQueueItem);
+
+
+            
+
+            // Queue the message to be sent in the background by adding it to the collector.
+            // If only the event is passed, an Event Hub partition to be be assigned via
+            // round-robin for each batch.
+            //await outputEvents.AddAsync(new EventData(newEventBody));
+
         }
     }
 }
